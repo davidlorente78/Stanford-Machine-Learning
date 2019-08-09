19c71800769a2fb3356da6d9e4002eec0c4259e7
David Lorente <davidlorente@outlook.com>
2019-08-09 21:56:23 +0200

    Neural Networkn Digit Recognition Predict

--- 03.Multi-Class-Neural-Networks/ex3/predict.m
+++ 03.Multi-Class-Neural-Networks/ex3/predict.m
@@ -21,8 +21,17 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X];
 
+z2 = a1*Theta1';
 
+a2 = [ones(size(z2, 1), 1) sigmoid(z2)];
+
+z3 = a2*Theta2';
+
+a3 = sigmoid(z3);
+
+[p_max, p] = max(a3')
 
 
 
